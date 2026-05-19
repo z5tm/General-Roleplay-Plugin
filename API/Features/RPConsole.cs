8792f6fd1f1584fe9768513543a821f684c9df91
Axeras <mallen0841@gmail.com>
2026-05-19 11:20:53 -0700

    guh

---
+++ API/Features/RPConsole.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using CommandSystem;
+using Exiled.API.Features;
+using GRPP;
+
+[CommandHandler(typeof(ClientCommandHandler))]
+public class RPConsole : ICommand
+{
+	public string Command { get; } = "rp";
+	public string[] Aliases { get; } = { "rpconsole" };
+	public string Description { get; } = "Allows you to send a roleplay broadcast to all players in the room with you.";
+
+    public void RoomBroadcast(Player player, ushort duration, string message)
+    {
+        Room currentroom = player.CurrentRoom;
+
+        foreach (Player target in Player.List)
+        {
+            if (target.CurrentRoom == currentroom)
+            {
+                target.Broadcast(Plugin.Singleton.Config.RPBroadcastDuration, $"{player.CustomName} says: " + message);
+            }
+        }
+    }
+
+    public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+	{
+		//rp [duration] [message]
+
+		if (arguments.Count == 0 || arguments.Count == 1)
+		{
+			response = "Usage: .rp [message]";
+			return false;
+		}
+
+		if (!ushort.TryParse(arguments.At(0), out ushort duration))
+		{
+			response = "Invalid duration. The first argument should be the duration of the broadcast in seconds.";
+			return false;
+		}
+
+		string message = string.Join(" ", arguments.Skip(1).ToArray());
+		RoomBroadcast(Player.Get(sender), duration, message);
+        response = "Roleplay message successfully sent!";
+		return true;
+    }
+}
