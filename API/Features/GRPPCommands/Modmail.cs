bd60e7af1290ffd46973f3b865ddec70d16a6ba8
Axeras <mallen0841@gmail.com>
2026-06-22 23:02:19 -0700

    the index thing, haven't tested

--- API/Features/GRPPCommands/Modmail.cs
+++ API/Features/GRPPCommands/Modmail.cs
@@ -3,7 +3,18 @@ using Exiled.API.Features;
 using GRPP;
 using GRPP.API.Core.Webhooks;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+
+public class ModmailHandler
+{
+    public static int index;
+    public static Dictionary<int, Player> modmailInstances = new Dictionary<int, Player>();
+
+    public static void InitEvents() => ServerHandlers.WaitingForPlayers += WaitingForPlayers;
+
+    private static void WaitingForPlayers() => index = 0;
+}
 
 [CommandHandler(typeof(ClientCommandHandler))]
 public class Modmail : ICommand
@@ -14,17 +25,24 @@ public class Modmail : ICommand
     public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
     {
         var player = Player.Get(sender);
+
         if (arguments.Count == 0)
         {
             response = "Usage: .mm [message]";
             return false;
         }
-        string message = string.Join(" ", arguments.ToArray());
+
+        ModmailHandler.index += 1;
+        var currentIndex = ModmailHandler.index;
+        ModmailHandler.modmailInstances.Add(currentIndex, player);
+
+        var message = string.Join(" ", arguments.ToArray());
+
         foreach (Player target in Player.List)
         {
             if (target.RemoteAdminAccess)
             {
-                target.SendConsoleMessage($"[ModMail] {player.DisplayNickname} ({player.UserId}): {message}", "yellow");
+                target.SendConsoleMessage($"[ModMail] {player.DisplayNickname} ({player.UserId}) (Modmail Index: {currentIndex}): {message}", "yellow");
                 target.Broadcast(10, $"New modmail from {player.DisplayNickname} ({player.UserId}). Check the client console!");
             }
         }
@@ -33,9 +51,45 @@ public class Modmail : ICommand
                 webhookNameToUse: "ModmailLogger",
                 webhookUrl: Plugin.Singleton.Config.ModmailCommandWebhookUrl,
                 title: "New ModMail Message",
-                description: $"A user has sent a modmail message.\nName: \"{player.DisplayNickname}\"\nSteamID64: \"{player.UserId}\"\nMessage: \"{message}\"",
+                description: $"A user has sent a modmail message. Index: {currentIndex}.\nName: \"{player.DisplayNickname}\"\nSteamID64: \"{player.UserId}\"\nMessage: \"{message}\"",
                 color: "088808");
         response = "Your message has been sent to the moderators!";
+        return true;
+    }
+}
+
+[CommandHandler(typeof(RemoteAdminCommandHandler))]
+public class Reply : ICommand
+{
+    public string Command { get; } = "reply";
+    public string[] Aliases { get; } = new string[] { "r" };
+    public string Description { get; } = "Replies to a modmail message using the index provided in the message.";
+    public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+    {
+        var player = Player.Get(sender);
+        if (arguments.Count < 2)
+        {
+            response = "Usage: .reply [index] [message]";
+            return false;
+        }
+        if (!int.TryParse(arguments.At(0), out int index))
+        {
+            response = "The index must be a valid integer.";
+            return false;
+        }
+        if (!ModmailHandler.modmailInstances.ContainsKey(index))
+        {
+            response = "No modmail message found with the provided index.";
+            return false;
+        }
+
+        var target = ModmailHandler.modmailInstances[index];
+        var message = string.Join(" ", arguments.Skip(1).ToArray());
+
+        target.SendConsoleMessage($"[ModMail Reply] {player.DisplayNickname}[Staff]: {message}", "yellow");
+        target.Broadcast(10, "You have received a reply to your modmail message. Check the client console!");
+
+        response = $"Reply sent to {target.DisplayNickname}!";
         return true;
     }
 }
